426a4af61b045d15ab2543340909a496a003aa9a
giangnguyen95 <giangnt_58@vnu.edu.vn>
2015-10-06 19:07:52 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Giang::Application.config.secret_key_base = '869f31a73ca3d9fd49d84f2ac0350f83b13afc952f15671a6ea769b5d39b927c22da27ed96b5f742bb11264cfd5e6ab87a2ff711361bd2d76f4eda3fc37a39be'
