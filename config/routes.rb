2ee9a54957d17ea2206b3ef7622c76ffc448ef00
giangnguyen95 <giangnt_58@vnu.edu.vn>
2015-10-19 09:47:08 +0700

    Finish log in/log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Giang::Application.routes.draw do
 
+  get "sessions/new"
   #get "static_pages/home"
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
   root 'static_pages#home'
   get 'signup'  => 'users#new' 
+  get 'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
   resources :users
   resources :microposts
   #root 'users#index'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,13 +5,36 @@ require "minitest/reporters"
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
-  ActiveRecord::Migration.check_pending!
+  #ActiveRecord::Migration.check_pending!
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   #
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
   # -- they do not yet inherit this setting
   fixtures :all
-
+  #Return true if a test user is logged in
+  def is_logged_in?
+  	!session[:user_id].nil?
+  end 
   # Add more helper methods to be used by all tests here...
+  #log in a test user
+  def log_in_as(user, options={})
+    password = options[:password] || 'password'
+    remember_me = options[:remember_me] || '1'
+    if integration_test?
+      post login_path, session: { email: user.email,
+                                  password: password,
+                                  remember_me: remember_me}
+    else
+      session[:user_id] = user_id
+    end
+  end
+
+  private
+
+  #returns true inside an intergration test
+  def integration_test?
+    defined?(post_via_redirect)
+  end
+    
 end
